0f217afaba8443de635a6397f52f53c2e6a5d6fa
Rahul Zalkikar <rayzck9@gmail.com>
2020-12-05 19:13:02 -0500

    adding memory

--- apps/dialog_process.s
+++ apps/dialog_process.s
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=4:00:00
-#SBATCH --mem=64GB
+#SBATCH --mem=128GB
 #SBATCH --job-name=dialog_states
 #SBATCH --mail-type=END
 #SBATCH --mail-user=rz1567@nyu.edu
