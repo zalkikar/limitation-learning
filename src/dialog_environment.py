abfc3456c07fc461ec6a8c1870490722a74ff576
Noah Kasmanoff <nkasmanoff@gmail.com>
2020-12-07 16:59:34 -0500

    silly imports gone

--- src/dialog_environment.py
+++ src/dialog_environment.py
@@ -11,15 +11,10 @@ to make our framework as similar as possible to previously successful approaches
 
 
 import torch
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+
 
 import torch
-from torch.jit import script, trace
 import torch.nn as nn
-from torch import optim
 import torch.nn.functional as F
 import csv
 import random
