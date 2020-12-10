016c93a44c9fea121bfef80bd6ac74393bc87459
Noah Kasmanoff <nkasmanoff@gmail.com>
2020-12-10 15:40:38 -0500

    arg parse

---
+++ src/models/run_gail.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --mem-per-cpu 80G
+#SBATCH --time 0-024:00:00
+#SBATCH --job-name run-gail
+#SBATCH --output slurm-%J.log 
+
+python main.py --logdir 'logs/v1'
