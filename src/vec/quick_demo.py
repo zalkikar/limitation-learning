9372139817be5126b287184ca053c165f6b09292
Rahul Zalkikar <rayzck9@gmail.com>
2020-12-10 21:22:32 -0500

    quick demo update

--- src/vec/quick_demo.py
+++ src/vec/quick_demo.py
@@ -1,7 +1,17 @@
 import torch
 
 d = torch.load('./dat/processed/padded_vectorized_states.pt') #, map_location=lambda storage, loc: storage.cuda(1))
+d2 = torch.load('./dat/processed/vectorized_states.pt')
 raw = torch.load('./dat/processed/raw_states.pt') #, map_location=lambda storage, loc: storage.cuda(1))
+
+for index, vects in d2.items():
+    input_state, next_state = vects[0], vects[1]
+    raw_input_state, raw_next_state = list(raw.keys())[index], raw[list(raw.keys())[index]]
+    print(raw_input_state,"\n", vects[0])
+    print(raw_next_state,"\n", vects[1])
+
+    if index > 1:
+        break
 
 for index, vects in d.items():
     input_state, next_state = vects[0], vects[1]
