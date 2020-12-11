db838512fb8c34752033a9eb96512fd63b783dcb
nkasmanoff <nkasmanoff@gmail.com>
2020-12-10 22:22:32 -0500

    updates t

--- src/run_gail.sh
+++ src/run_gail.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:v100:1
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
-#SBATCH --mem-per-cpu 10G
+#SBATCH --mem-per-cpu 150G
 #SBATCH --time 0-024:00:00
 #SBATCH --job-name run-gail
 #SBATCH --output slurm-%J.log 
 
-python main.py --logdir 'logs/v1'
+python main.py --logdir 'logs/v5' --actor_critic_update_num 100 --discrim_update_num 50 --batch_size 32 --hidden_size 8 --num_layers 4
