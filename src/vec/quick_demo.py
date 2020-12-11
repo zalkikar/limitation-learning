61f98cf6e815f7e1ce6f970d2760c4160d504151
Rahul Zalkikar <rayzck9@gmail.com>
2020-12-10 22:23:52 -0500

    w2v demo output

--- src/vec/quick_demo.py
+++ src/vec/quick_demo.py
@@ -1,5 +1,15 @@
 import torch
+import gensim
 
+model = gensim.models.Word2Vec.load("./models/custom_w2v_intersect_GoogleNews")
+model.init_sims(replace=True) #precomputed l2 normed vectors in-place – saving the extra RAM
+print(model.vocabulary.sorted_vocab) # should be True
+print(model.wv.vectors.shape)
+print(list(model.wv.vocab.keys()))
+print(model_vects.shape)
+
+
+"""
 d = torch.load('./dat/processed/padded_vectorized_states.pt') #, map_location=lambda storage, loc: storage.cuda(1))
 d2 = torch.load('./dat/processed/vectorized_states.pt')
 raw = torch.load('./dat/processed/raw_states.pt') #, map_location=lambda storage, loc: storage.cuda(1))
@@ -21,3 +31,4 @@ for index, vects in d.items():
 
     if index > 1:
         break
+"""
